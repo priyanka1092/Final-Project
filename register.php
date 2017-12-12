d2f468a451f361718d7bfad83468411121527c4a
priyanka nilesh bongale <pb435@afsconnect1.njit.edu>
2017-12-12 15:12:49 -0500

    changes in register page

--- register.php
+++ register.php
@@ -22,9 +22,6 @@
     First name: <input type="text" name="fname"><br>
     Last name: <input type="text" name="lname"><br>
     Email: <input type="text" name="email"><br>
-    Phone: <input type="text" name="phone"><br>
-    Birthday: <input type="text" name="birthday"><br>
-    Gender: <input type="text" name="gender"><br>
     Password: <input type="password" name="password"><br>
     <input type="submit" value="Submit form">
 </form>
