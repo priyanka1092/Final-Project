2f9601744385ba2d3109a100fadc679f49b73334
priyanka nilesh bongale <pb435@afsconnect1.njit.edu>
2017-12-17 22:03:32 -0500

    UI changes

--- pages/home.php
+++ pages/home.php
@@ -8,30 +8,28 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <style>
-    /* Remove the navbar's default margin-bottom and rounded borders */ 
+   
     .navbar {
       margin-bottom: 0;
       border-radius: 0;
     }
     
-    /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
+    
     .row.content {height: 650px}
     
-    /* Set gray background color and 100% height */
     .sidenav {
       padding-top: 20px;
       background-color: #f1f1f1;
       height: 100%;
     }
     
-    /* Set black background color, white text and some padding */
     footer {
       background-color: #555;
       color: white;
       padding: 15px;
     }
     
-    /* On small screens, set height to 'auto' for sidenav and grid */
+    /
     @media screen and (max-width: 767px) {
       .sidenav {
         height: auto;
@@ -87,7 +85,7 @@
       
       <ul class="navbar-form navbar-right">
         <form action="index.php?page=accounts&action=show" method="POST">
-          <button type="submit" class="btn btn-default">Your Profile</button>
+          <button type="submit" class="btn btn-default">Profile</button>
       </form>
       <form action="index.php?page=accounts&action=logout" method="POST">
         <button type="submit" class="btn btn-default">Logout</button>
@@ -98,7 +96,10 @@
   </div>
 </nav>
 
-<p>Home Page</p>
+<p><H2>Hello,</H2></p>
+<p> <H3>Welcome to Task Manager!! We provide a convenient way of managing tasks for our Users<H3><p>
+  <p> <h3>Sign up now to manage your Tasks. $10 Amazon gift card for New Users!! </h3></p>
+
 
 <script src="js/scripts.js"></script>
 </body>
